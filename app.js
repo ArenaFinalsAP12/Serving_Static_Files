8f0616dbf61b4c2221961495b403ec18aede846f
Atsushi <atsushi.idobe86@gmail.com>
2024-03-18 14:40:11 -0400

    These are the changes for the Http post request w/ Express and Body Parser Module that have a slight issue.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Serving Static Files with Express
+/*
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -6,3 +8,47 @@ app.get('/',(req,res)=> { // for the static folder changing its name to "public"
     res.sendFile(path.join(__dirname,'static','index.html'));
 });
 app.listen(3000);
+*/
+// Http post request with Express and Body Parser Module
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use('/public',express.static(path.join(__dirname,'static'))); // middleware
+app.use(bodyParser.urlencoded({extended: false})); // parses URL-encoded data and attaches it
+                                                   // to the request body
+app.get('/',(req,res)=> {
+    res.sendFile(path.join(__dirname,'static','indexform.html'));
+});
+app.post('/',(req,res)=> {
+    console.log(req.body);
+    // database work here
+    res.send('successfully posted data');
+});
+app.listen(3000);
+
+// Working with JSON Data - Express and Body Parser
+// Completed most of the new code in indexform.html file
+/*
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use('/public',express.static(path.join(__dirname,'static')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); // Parses json and attaches the response to bodyParser object
+
+app.get('/',(req,res)=> {
+    res.sendFile(path.join(__dirname,'static','indexform.html'));
+});
+
+app.post('/',(req,res)=> {
+    console.log(req.body);
+    // database work here
+    res.json({success : true}); // Takes a Javascript object and converts it into json
+});
+app.listen(3000);
+*/
