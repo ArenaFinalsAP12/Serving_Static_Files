d2d365b95f17bbdd6d677173f462b14e2040e16f
Atsushi <atsushi.idobe86@gmail.com>
2024-03-19 11:07:16 -0400

    Changed the indexform.html file to include script code for the next section covering JSON Data using Express and BodyParser

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ app.get('/',(req,res)=> { // for the static folder changing its name to "public"
 app.listen(3000);
 */
 // Http post request with Express and Body Parser Module
-
+/*
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use('/public',express.static(path.join(__dirname,'static'))); // middleware
-app.use(bodyParser.urlencoded({extended: false})); // parses URL-encoded data and attaches it
-                                                   // to the request body
+app.use(bodyParser.urlencoded({extended: false})); // parses URL-encoded data and attaches it to the request body
+
 app.get('/',(req,res)=> {
     res.sendFile(path.join(__dirname,'static','indexform.html'));
 });
@@ -28,10 +28,10 @@ app.post('/',(req,res)=> {
     res.send('successfully posted data');
 });
 app.listen(3000);
-
+*/
 // Working with JSON Data - Express and Body Parser
 // Completed most of the new code in indexform.html file
-/*
+
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -51,4 +51,3 @@ app.post('/',(req,res)=> {
     res.json({success : true}); // Takes a Javascript object and converts it into json
 });
 app.listen(3000);
-*/
